c35095bd50efa84dc17769d2b5fdd76bbe1dbf51
tva050 <91067164+tva050@users.noreply.github.com>
2021-10-04 13:55:15 +0200

    Branch

--- Mestringstest2a.py
+++ Mestringstest2a.py
@@ -7,23 +7,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-# fysiske parametre
-b = a = 3.3
-
-# r_a=r/a=r/b
-r_a = np.arange(0.5, 3, 0.001)
+# r=r/a=r/b
+r = np.arange(0.5, 3, 0.001)
 
 
-# Lennard-Jones potensialet funksjon
-def lj_p(r_a):
-    func = ((a/r_a)**(12)-(b/r_a)**(6))
+# Lennard-Jones potential
+def lj_p(r):
+    
+    
+    func = ((r)**(-12)-(r)**(-6))
+    
     return func
 
 
-plt.plot(r_a,lj_p(r_a))
-plt.xlabel("r/a ")
-plt.ylabel("ljp")
+plt.plot(r,lj_p(r))
+plt.xlabel("r/a")
+plt.ylabel("v/v0")
 plt.ylim((-0.25,0.1))
 plt.show
-    
 
