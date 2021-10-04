1ff1000e83c0bd557cd4702b61ad1d318bbcadb4
tva050 <91067164+tva050@users.noreply.github.com>
2021-10-04 19:52:31 +0200

    working code

--- Mestringstest2a.py
+++ Mestringstest2a.py
@@ -13,7 +13,10 @@ r = np.arange(0.5, 3, 0.001)
 
 # Lennard-Jones potential
 def lj_p(r):
-    
+    """
+    Redefining the Lennard-Jones potential (v = v0 ((a/r)**12 - (b/r)**6)) to, 
+    write the function for v/v0 against r/a.
+    """
     
     func = ((r)**(-12)-(r)**(-6))
     
@@ -21,8 +24,11 @@ def lj_p(r):
 
 
 plt.plot(r,lj_p(r))
+plt.axhline(y=0, color='r', linestyle='--')
+plt.title("Plot v/v0 against r/a")
 plt.xlabel("r/a")
-plt.ylabel("v/v0")
+plt.ylabel(r'$v/v_0$')
 plt.ylim((-0.25,0.1))
-plt.show
+plt.show()
+
 
